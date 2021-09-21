57a4b38da3655b43947b03c12225721e87d8b337
AyshathSafana <91135550+AyshathSafana@users.noreply.github.com>
2021-09-21 19:42:39 +0530

    day1 assignment to calculate simple interest

---
+++ day1.py
@@ -0,0 +1,8 @@
+p=float(input("enter the principle:"))
+r=float(input("enter the rate:"))
+t=float(input("enter the time:"))
+print("principle:",p)
+print("rate:",r)
+print("time:",t)
+si=(p*r*t)/100
+print("simple interest:",si)
